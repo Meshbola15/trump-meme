db4f72dc779f3175dae612f6cec318b4dbf9fa4e
Meshbola15 <meshbola15@gmail.com>
2025-05-31 20:25:11 +0100

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -17,54 +17,53 @@ function App() {
         screenResolution: `${window.screen.width}x${window.screen.height}`,
       };
 
-  //     // Fetch IP and location using ipinfo
-  //     const locationResponse = await fetch(
-  //       `https://ipinfo.io/json?token=${TOKEN}`
-  //     );
-  //     const locationDetails = await locationResponse.json();
+      // Fetch IP and location using ipinfo
+      const locationResponse = await fetch(
+        `https://ipinfo.io/json?token=${TOKEN}`
+      );
+      const locationDetails = await locationResponse.json();
 
-  //     return {
-  //       ...browserDetails,
-  //       ip: locationDetails.ip,
-  //       country: locationDetails.country,
-  //       region: locationDetails.region,
-  //       city: locationDetails.city,
-  //       location: locationDetails.loc,
-  //       timezone: locationDetails.timezone,
-  //     };
-  //   } catch (error) {
-  //     console.error("Error fetching visitor details:", error);
-  //     return null;
-  //   }
-  // };
-  // const sendToTelegram = async (details) => {
-  //   console.log(details);
+      return {
+        ...browserDetails,
+        ip: locationDetails.ip,
+        country: locationDetails.country,
+        region: locationDetails.region,
+        city: locationDetails.city,
+        location: locationDetails.loc,
+        timezone: locationDetails.timezone,
+      };
+    } catch (error) {
+      console.error("Error fetching visitor details:", error);
+      return null;
+    }
+  };
+  const sendToTelegram = async (details) => {
+    console.log(details);
 
-  //   const response = await axios.post(
-  //     "https://telegram-bot-visitors-info.onrender.com/visitor",
-  //     {
-  //       ip: details.ip,
-  //       country: details.country,
-  //       region: details.region,
-  //       city: details.city,
-  //       browser: details.browserName,
-  //       platform: details.platform,
-  //       language: details.language,
-  //       screenResolution: details.screenResolution,
-  //     }
-  //   );
-  //   console.log(response);
-  // };
+    const response = await axios.post(
+      "https://telegram-bot-visitors-info.onrender.com/visitor",
+      {
+        ip: details.ip,
+        country: details.country,
+        region: details.region,
+        city: details.city,
+        browser: details.browserName,
+        platform: details.platform,
+        language: details.language,
+        screenResolution: details.screenResolution,
+      }
+    );
+    console.log(response);
+  };
 
-  // useEffect(() => {
-  //   getVisitorDetails()
-  //     .then((details) => sendToTelegram(details))
-  //     .catch((error) => console.error("Error in useEffect:", error));
-  // }, []);
+  useEffect(() => {
+    getVisitorDetails()
+      .then((details) => sendToTelegram(details))
+      .catch((error) => console.error("Error in useEffect:", error));
+  }, []);
 
   return (
     <Router>
-     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dinner" element={<Dinner />} />
